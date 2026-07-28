79c131bdc75bdddae44d826bb60beb4e1faaf104
andresvp17 <andres.aevp68@gmail.com>
2026-07-28 11:02:33 -0400

    delete import line

--- src/main/java/ui/PayslipPanel.java
+++ src/main/java/ui/PayslipPanel.java
@@ -17,7 +17,6 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.List;
-import java.util.stream.IntStream;
 
 public class PayslipPanel extends JPanel {
     private final DefaultTableModel tableModel;
